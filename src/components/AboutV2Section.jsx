d241f5c59b1cb1185e263bbe98fbde98b05c8d5f
mpii <ferry4biz@gmail.com>
2025-07-07 18:54:09 +0700

    Update AboutV2Section component: change title from 'GREYOLA' to 'KELVIANOV' and enhance CSS for improved typography and layout

--- src/components/AboutV2Section.jsx
+++ src/components/AboutV2Section.jsx
@@ -10,7 +10,9 @@ const AboutV2Section = () => {
         <span className="aboutv2-section-since">SINCE 2025</span>
       </div>
       <div className="aboutv2-section-content">
-        <h1 className="aboutv2-section-title-main">MORE ABOUT<br/>GREYOLA<span className="aboutv2-section-copyright">©</span></h1>
+        <h1 className="aboutv2-section-title-main">
+          MORE ABOUT<br/>KELVIANOV<span className="aboutv2-section-copyright">©</span>
+        </h1>
       </div>
     </section>
   );
